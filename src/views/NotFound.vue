84811b06acd1a5923f81d60df5efce3b60c10147
amilovanovikj <andrejmilovanovic98@gmail.com>
2021-09-11 12:14:50 +0200

    Apply the same styles from home to not-found view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,16 +1,34 @@
 <template>
-  <section class="hero is-fullheight-with-navbar is-dark">
+  <section class="hero is-fullheight-with-navbar is-info">
+    <div class="background-wrap">
+      <Bubbles />
+    </div>
     <div class="hero-body">
-      <div>
+      <div class="container auth-container has-text-centered">
         <p class="title">whoops!</p>
         <p class="subtitle">looks like this page doesn't exist...</p>
+        <button
+          class="button is-dark is-fullwidth has-text-white"
+          @click="home()"
+        >
+          <span>home</span>
+        </button>
       </div>
     </div>
   </section>
 </template>
 
-<script>
-export default {
-  name: "NotFound"
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import Bubbles from "../components/Bubbles.vue"
+
+export default defineComponent({
+  name: "NotFound",
+  components: { Bubbles },
+  methods: {
+    home(){
+      this.$router.push('home')
+    },
+  },
+});
 </script>

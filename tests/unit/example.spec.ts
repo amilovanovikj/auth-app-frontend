54ad3c839dd16cce9693e168587c7a28f5bc8809
amilovanovikj <andrejmilovanovic98@gmail.com>
2021-09-29 19:24:50 +0200

    Add new test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import RetypePasswordField from "@/components/RetypePasswordField.vue";
+import EmailField from "@/components/EmailField.vue";
 
 describe("RetypePasswordField.vue", () => {
   it("renders message when isRetypePasswordInvalid is true", () => {
@@ -12,5 +13,18 @@ describe("RetypePasswordField.vue", () => {
       }
     });
     expect(wrapper.text()).to.include("passwords must match");
+  });
+});
+
+describe("EmailField.vue", () => {
+  it("renders message when isEmailInvalid is true", () => {
+    const wrapper = shallowMount(EmailField, {
+      data () {
+        return {
+          isEmailInvalid: true
+        }
+      }
+    });
+    expect(wrapper.text()).to.include("please specify a valid email address");
   });
 });

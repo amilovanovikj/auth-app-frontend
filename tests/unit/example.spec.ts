d3fff706fca5270cb293410d9d27f2b312f7f7d1
Andrej Milovanovikj <47496421+amilovanovikj@users.noreply.github.com>
2021-10-07 22:31:47 +0200

    Develop: Ready for testing (#3)
    * Feature: CI/CD (#1)
    
    * Create pipeline.yml
    
    * CI/CD pipeline for Docker deployment on Azure Web App
    ** Create .env from CI/CD
    ** Build Docker image and push to ACR
    ** Deploy Docker container to Web App
    
    * Deploy to Web App using SPN instead of publish profile
    
    * Add .github folder in .dockerignore
    
    * Fix Web App name in Deploy step
    
    * Readability improvements in Dockerfile
    
    * Reorder tasks in CI/CD pipeline
    
    * Also tag new Docker images with 'latest' in CI/CD pipeline
    
    * Change Docker image tag
    
    * Add SonarQube to the project
    ** Add SonarCloud scan task in CI/CD
    ** Add sonar-project.properties file
    
    * Dynamically add and remove GitHub Actions runner IP to ACR firewall
    
    * Remove duplicate line in styles.scss
    
    * Allways run 'Remove runner IP from ACR firewall' and 'Az logout' tasks in CI/CD
    
    * Add SonarQube Code coverage report option and a sample test
    
    * Fix code coverage in CI/CD
    
    * Update CI/CD to trigger only on specified branch
    
    * Run SonarQube scan with tests
    
    * Add new test
    
    * Remove test coverage
    
    * Add sample integration test
    
    * Add GitHub Actions pipelines for each environment
    
    * Fix: CI/CD (#2)
    
    * Remove unused CI/CD pipeline
    
    * Remove run of integration tests for production CI/CD pipeline
    
    * Fix Dockerfile vulnerabilities found by Snyk
    
    * Fix Git errors

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,30 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import RetypePasswordField from "@/components/RetypePasswordField.vue";
+import EmailField from "@/components/EmailField.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe("RetypePasswordField.vue", () => {
+  it("renders message when isRetypePasswordInvalid is true", () => {
+    const wrapper = shallowMount(RetypePasswordField, {
+      data () {
+        return {
+          isRetypePasswordInvalid: true
+        }
+      }
     });
-    expect(wrapper.text()).to.include(msg);
+    expect(wrapper.text()).to.include("passwords must match");
+  });
+});
+
+describe("EmailField.vue", () => {
+  it("renders message when isEmailInvalid is true", () => {
+    const wrapper = shallowMount(EmailField, {
+      data () {
+        return {
+          isEmailInvalid: true
+        }
+      }
+    });
+    expect(wrapper.text()).to.include("please specify a valid email address");
   });
 });

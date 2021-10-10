8cac6a889d09d29011bc401cc577223908b38ea3
amilovanovikj <andrejmilovanovic98@gmail.com>
2021-10-10 15:43:24 +0200

    Add client side monitoring

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+
+const appInsights = new ApplicationInsights({
+  config: {}
+});
+appInsights.loadAppInsights();
+appInsights.trackPageView();
 
 createApp(App)
   .use(store)
